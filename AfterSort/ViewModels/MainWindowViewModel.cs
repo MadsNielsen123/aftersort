8ba46ee6cd8e39d0abaec4c6ba5280a0ac620836
Mads Nielsen <madsn2000@gmail.com>
2026-05-29 21:56:31 +0200

    InitialSetup

---
+++ AfterSort/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,6 @@
+﻿namespace AfterSort.ViewModels;
+
+public partial class MainWindowViewModel : ViewModelBase
+{
+    public string Greeting { get; } = "Welcome to Avalonia!";
+}
---
+++ AfterSort/ViewModels/ViewModelBase.cs
@@ -0,0 +1,7 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace AfterSort.ViewModels;
+
+public abstract class ViewModelBase : ObservableObject
+{
+}
